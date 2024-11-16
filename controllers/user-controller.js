79fe6ff59473e87c54cd251ce537fdc6c8829879
weser42 <58133977+weser42@users.noreply.github.com>
2024-11-16 23:37:24 +0400

    feat: added user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,8 @@
-const { prisma } = require("@prisma/client");
+const { prisma } = require("../prisma/prisma-client");
 const bcrypt = require("bcryptjs");
+const jdenticon = require("jdenticon");
+const path = require("path");
+const fs = require("fs");
 
 const UserController = {
   register: async (req, res) => {
@@ -10,12 +13,28 @@ const UserController = {
     }
     try {
       const existingUser = await prisma.user.findUnique({ where: { email } });
-
       if (existingUser) {
         return res.status(400).json({ error: "the user already exists" });
       }
       const hashedPassword = await bcrypt.hash(password, 10);
-    } catch (error) {}
+      const png = jdenticon.toPng(name, 200); //generate img
+      const avatarName = `${name}_${Date.now()}.png`;
+      const avatarPath = path.join(__dirname, "/../uploads", avatarName);
+      fs.writeFileSync(avatarPath, png);
+      const user = await prisma.user.create({
+        data: {
+          email,
+          password: hashedPassword,
+          name,
+          avatarUrl: `/uploads/${avatarPath}`,
+        },
+      });
+
+      res.json(user);
+    } catch (error) {
+      console.error("Error in register", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   },
   login: async (req, res) => {
     res.send("login");
