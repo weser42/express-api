c0d4f86225295b2c2963c19926a26137e3a71b7f
weser42 <58133977+weser42@users.noreply.github.com>
2024-11-16 22:12:35 +0400

    feat: hashing a password using the bicrypt library

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,21 @@
+const { prisma } = require("@prisma/client");
+const bcrypt = require("bcryptjs");
+
 const UserController = {
   register: async (req, res) => {
-    res.send("register");
+    const { email, password, name } = req.body;
+
+    if (!email || !password || !name) {
+      return res.status(400).json({ error: "all fields are required" });
+    }
+    try {
+      const existingUser = await prisma.user.findUnique({ where: { email } });
+
+      if (existingUser) {
+        return res.status(400).json({ error: "the user already exists" });
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+    } catch (error) {}
   },
   login: async (req, res) => {
     res.send("login");
