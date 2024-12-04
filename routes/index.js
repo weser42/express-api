4b19e8d4309c646d103d20bbd6e3f1277202a98f
Gennady Anisimow <anisgennady@gmail.com>
2024-12-04 22:03:50 +0400

    fix: bug fixed "[nodemon] app crashed "

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer"); //install: npm i multer
 const { UserController } = require("../controllers");
+const PostController = require("../controllers/post-controller");
 const { authenticateToken } = require("../middleware/auth");
 
 const uploadDestination = "uploads";
