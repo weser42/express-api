1da050f97bf2ed1ab523a53699beccd12cdbb9e9
Gennady Anisimow <anisgennady@gmail.com>
2024-12-04 21:06:29 +0400

    feat: development of the user controller module has been completed

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -131,7 +131,32 @@ const UserController = {
     }
   },
   current: async (req, res) => {
-    res.send("current");
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: req.user.userId },
+        include: {
+          followers: {
+            include: {
+              follower: true,
+            },
+          },
+          following: {
+            include: {
+              following: true,
+            },
+          },
+        },
+      });
+
+      if (!user) {
+        return res.status(400).json({ error: "Could not find user" });
+      }
+
+      return res.status(200).json(user);
+    } catch (error) {
+      console.log("err", error);
+      res.status(500).json({ error: "Something went wrong" });
+    }
   },
 };
 
